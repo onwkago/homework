78e7f4aa68b756cf4a777317b4cd620f400ad9be
WCC Academy <uiacademy@kayak.com>
2021-02-01 17:41:29 +0200

    Update README.md

---
+++ app/src/components/DropDown/DropDown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {InputGroup} from "reactstrap";
+
+const DropDown = () => {
+
+    return (
+        <React.Fragment>
+           <div>
+               <div>Dropdown</div>
+               <div>Dropdown</div>
+               <div>Dropdown</div>
+               <div>Dropdown</div>
+               <div>Dropdown</div>
+               <div>Dropdown</div>
+           </div>
+        </React.Fragment>
+    );
+};
+
+export default DropDown;
---
+++ app/src/components/PageLayout/PageLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PageLayout = () => {
+
+    return (
+        <React.Fragment>
+            <div className={'pageLayout'}>
+                
+            </div>
+        </React.Fragment>
+    );
+
+};
+
+export default PageLayout;
